31d87bb567ec7a0ee7c1e4ab78cda564c0dc63d8
Nethmin Dulsara <dulsaranethmin@gmail.com>
2021-10-05 18:23:03 +0530

    component folder added

---
+++ src/components/helsinki.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import helsinki_mibile from './helsinki-mobile.jpeg';
+import flame from './flame-2x700.jpg';
+
+function Helsinki(){
+    return(
+<div className="container-xxl mt-2 d-float  w-100 h-100">
+<nav className="navbar navbar-light bg-light  mb-5">
+  <div className="container-fluid d-sm-inline">
+    <a className="navbar-brand text-center fw-bold fs-3 text-primary" href="#">
+      IEEE Open Day 2021
+    </a>
+  </div>
+</nav>
+  <div className="text-center">
+        <div class="row align-items-center d-none d-sm-flex">
+            <div class="col">
+            <img src={flame} class="rounded d-none d-sm-inline" alt="..."/>
+            </div>
+            <div class="col">
+              <div class="card">
+                <div class="card-header">
+                  Happy Helsinki
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Don't trust your eye ! Snake is moving...</h5>
+                  <p class="card-text"></p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+      </div>
+      
+      <div className="card d-sm-none">
+        <img src={helsinki_mibile} class="card-img-top" alt="..."/>
+        < div className="card-body">
+        <h5 className="card-title">Happy Helsinki</h5>
+        <p className="card-text">Don't trust your eye ! Snake is moving...</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+  </div>
+</div>
+    );
+}
+
+export default Helsinki;
