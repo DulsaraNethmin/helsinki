f3ea68ea3ecb0dd8e05e9f71b640a720b4c62047
Nethmin Dulsara <dulsaranethmin@gmail.com>
2021-10-05 09:54:20 +0530

    sa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
 import React, { StrictMode } from 'react';
 import hr from './hr.jpeg';
+import sa from './s.jpeg'; 
 
 function App() {
   return (
-    <div className='container-sm'>
-      <div class="card my-4 p">
+<div className='container-sm'>
+  <div class="card my-4 p">
   <img src={hr} class="card-img-top" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">Mawathagama Harsha</h5>
     <p class="card-text">I'm Harsha Amarasighe.Maliyadewa Collage - Kurunagala</p>
     <a href="#" class="btn btn-primary">Chat with me</a>
   </div>
+  </div>
+
+  <div class="card my-4 p">
+  <img src={sa} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Kalani Sangeeth</h5>
+    <p class="card-text">hi,I'm Sangeeth Navodya.</p>
+    <a href="#" class="btn btn-primary">Chat with me</a>
+  </div>
+  </div>
 </div>
-    </div>
   );
 }
 
