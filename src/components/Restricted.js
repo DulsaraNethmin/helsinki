53c3ec687c878f7ec752d2c90756ddd42dc95c23
Nethmin Dulsara <dulsaranethmin@gmail.com>
2021-10-05 19:51:40 +0530

    restricted set

--- src/components/Restricted.js
+++ src/components/Restricted.js
@@ -1,10 +1,29 @@
 import React from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import flame from './flame-2x700.jpg';
+import question from './question.png';
 
 function Restricted(){
     return(
-        <>
-            hi
-        </>
+        <div className="container-xxl mt-2 d-float mt-6 w-100 h-100">
+                <div class="row align-items-center d-none d-sm-flex">
+                    <div class="col">
+                    <img src={question} class="rounded d-none d-sm-inline" alt="..."/>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-header text-danger">
+                            This is Restricted Area
+                            </div>
+                            <div class="card-body">
+                            <h5 class="card-title">Finish the task and take your screen shot !</h5>
+                            <p class="card-text"></p>
+                            <a href="#" class="btn btn-primary"><Link to='/' className='text-light'>Go Back</Link></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>     
+        </div>
     );
 } 
 
