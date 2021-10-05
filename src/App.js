75e63122b060c4179938784203f99089e776c268
Nethmin Dulsara <dulsaranethmin@gmail.com>
2021-10-05 16:09:49 +0530

    nav add

--- src/App.js
+++ src/App.js
@@ -6,10 +6,26 @@ import helsinki_mibile from './helsinki-mobile.jpeg';
 
 function App() {
   return (
-<div className="container-xxl mt-5 d-float  text-center w-100 h-100">
-  <div class="text-center">
-      <img src={helsinki_1000} class="rounded d-none d-sm-inline" alt="..."/>
-      <div className="card d-sm-none">
+
+<div className="container-xxl mt-5 d-float  w-100 h-100">
+<nav className="navbar navbar-light bg-light  ">
+  <div className="container-fluid d-sm-inline">
+    <a className="navbar-brand text-center fw-bold fs-3 text-primary" href="#">
+      IEEE Open Day 2021
+    </a>
+  </div>
+</nav>
+  <div className="text-center">
+        <div class="row align-items-start d-none d-sm-flex">
+            <div class="col">
+            <img src={helsinki_1000} class="rounded d-none d-sm-inline" alt="..."/>
+            </div>
+            <div class="col">
+              One of three columns
+            </div>
+      </div>
+      
+      <div className="cardd-sm-none">
         <img src={helsinki_mibile} class="card-img-top" alt="..."/>
         < div className="card-body">
         <h5 className="card-title">Helsinki</h5>
