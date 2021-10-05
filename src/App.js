a4f18a163df05c2477195245883e6f1cef6f02ec
Nethmin Dulsara <dulsaranethmin@gmail.com>
2021-10-05 10:04:32 +0530

    vr

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { StrictMode } from 'react';
 import hr from './hr.jpeg';
 import sa from './s.jpeg'; 
+import vr from './vira.jpeg';
 
 function App() {
   return (
@@ -21,6 +22,15 @@ function App() {
     <h5 class="card-title">Kalani Sangeeth</h5>
     <p class="card-text">hi,I'm Sangeeth Navodya.</p>
     <a href="#" class="btn btn-primary">Chat with me</a>
+  </div>
+  </div>
+
+  <div class="card my-4 p">
+  <img src={vr} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Vingara Bandara</h5>
+    <p class="card-text">I'm Viranga Bandara from Mawanalla</p>
+    <a href="#" class="btn btn-primary">Chat with me(ses)</a>
   </div>
   </div>
 </div>
