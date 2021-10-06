4e38462d45cecbebcbe4b54bf7020620a33ec7d7
Nethmin Dulsara <dulsaranethmin@gmail.com>
2021-10-06 09:33:19 +0530

    helsi purple add

--- src/components/CodeTwo.js
+++ src/components/CodeTwo.js
@@ -2,20 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import star from './star.jpeg';
 import helsinki_mibile from './helsinki-mobile.jpeg';
+import helsi_purple from './helsinki-ppt-1.x650.jpeg';
 function CodeTwo(){
     return(
 <div className="container-xxl mt-2 d-float  w-100 h-100">
             <div class="row align-items-center d-none d-sm-flex">
                     <div class="col">
-                    <img src={star} class="rounded d-none d-sm-inline" alt="..."/>
+                    <img src={helsi_purple} class="rounded d-none d-sm-inline" alt="..."/>
                     </div>
                     <div class="col">
                         <div class="card">
                             <div class="card-header text-danger">
-                            This is Restricted Area
+                            You're wasting your time!
                             </div>
                             <div class="card-body">
-                            <h5 class="card-title">Add today</h5>
+                            <h5 class="card-title">Count everything to survive</h5>
                             <p class="card-text"></p>
                             <a href="#" class="btn btn-primary"><Link to='/' className='text-light'>Go Back</Link></a>
                             </div>
@@ -24,10 +25,10 @@ function CodeTwo(){
                 </div>  
 
             <div className="card d-sm-none">
-            <img src={helsinki_mibile} class="card-img-top" alt="..."/>
+            <img src={helsi_purple} class="card-img-top" alt="..."/>
             < div className="card-body">
-            <h5 className="card-title">This is Restricted Area</h5>
-            <p className="card-text">Finish the task and take your screen shot !</p>
+            <h5 className="card-title">You're wasting your time!</h5>
+            <p className="card-text">Count everything to survive</p>
             <a href="#" className="btn btn-primary"><Link to='/' className='text-light'>Go Back</Link></a>
             </div>
             </div>   
